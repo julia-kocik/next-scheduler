835637d71334388aee2cf8c7a93cafcce3e176a7
julia-kocik <julia.kocik@gmail.com>
2023-11-12 12:16:26 +0100

    Add form validation

--- app/components/EventForm/eventForm.tsx
+++ app/components/EventForm/eventForm.tsx
@@ -1,12 +1,16 @@
 import React, { ChangeEvent, Dispatch, FormEvent, SetStateAction, useState } from 'react';
 import axios from 'axios';
 import styles from './eventForm.module.scss';
+import Toast from '../Toast/Toast';
+import { formatDateForInput } from 'app/utils/dataFormatter';
+import { showToast } from 'app/utils/showToast';
+import { emailRegex } from 'app/utils/emailRegex';
 
 interface FormFields {
   name: string;
   surname: string;
   email: string;
-  date: Date;
+  date: Date | string;
 }
 
 interface EventFormInterface {
@@ -18,9 +22,11 @@ export default function EventForm({ setForceFetch }: EventFormInterface) {
     name: '',
     surname: '',
     email: '',
-    date: new Date(),
+    date: '',
   });
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [toastInfo, setToastInfo] = useState({message: '', color: 'red'})
+  const { message, color } = toastInfo;
   const fieldsArray = Object.entries(formFields);
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -31,19 +37,22 @@ export default function EventForm({ setForceFetch }: EventFormInterface) {
       [name]: name === 'date' ? formatDateForInput(value) : value,
     }));
   };
-  
-  const formatDateForInput = (value: string): string => {
-    const selectedDate = new Date(value);
-    const year = selectedDate.getFullYear();
-    const month = (selectedDate.getMonth() + 1).toString().padStart(2, '0');
-    const day = selectedDate.getDate().toString().padStart(2, '0');
-  
-    return `${year}-${month}-${day}`;
-  };
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
     if (isSubmitting) {
+      return;
+    }
+    
+    const emptyFields = fieldsArray.filter(([_key, value]) => !value).map(([key]) => key)
+    if (emptyFields.length > 0) {
+      const emptyFieldsMessage = `Please fill in the following fields: ${emptyFields.join(', ')}.`;
+      showToast(emptyFieldsMessage, 'red', setToastInfo)
+      return;
+    }
+    if(!emailRegex.test(formFields.email)) {
+      showToast('Please provide valid email', 'red', setToastInfo)
       return;
     }
     try {
@@ -51,8 +60,9 @@ export default function EventForm({ setForceFetch }: EventFormInterface) {
       await axios.post('http://scheduler-env.eba-di2rddya.eu-north-1.elasticbeanstalk.com/api/v1/event', {
         ...formFields
       })
-    } catch (error) {
-      console.error('Error fetching data:', error);
+      showToast('Event successfully created!', 'green', setToastInfo)
+    } catch (error: unknown) {
+      showToast('Error occured when submitting data, please try again later.', 'red', setToastInfo)
     } finally {
       setIsSubmitting(false)
       setForceFetch(true)
@@ -61,21 +71,24 @@ export default function EventForm({ setForceFetch }: EventFormInterface) {
       name: '',
       surname: '',
       email: '',
-      date: new Date(),
+      date: '',
     })
   };
 
   return (
-    <form className={styles.container} onSubmit={onSubmit}>
-      {fieldsArray.map(el => {
-        const [key, value] = el;
-        return (
-          <div>
-            <label htmlFor={key}>{key[0].toUpperCase() + key.slice(1)}: </label>
-            <input type={key !== 'date' ? 'text' : 'date'} value={value} id={key} name={key} onChange={onChange} />
-          </div>
-      )})}
-      <button type="submit">Submit</button>
-    </form>
+    <div className={styles.container}>
+      {message && <Toast message={message} color={color}/>}
+      <form className={styles.container} onSubmit={onSubmit}>
+        {fieldsArray.map((el, index) => {
+          const [key, value] = el;
+          return (
+            <div key={index}>
+              <label htmlFor={key}>{key[0].toUpperCase() + key.slice(1)}: </label>
+              <input type={key !== 'date' ? 'text' : 'date'} value={value} id={key} name={key} onChange={onChange} />
+            </div>
+        )})}
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
