5cd72d6ad9d3bcde96793e75ae3ecead1ca6694c
julia-kocik <julia.kocik@gmail.com>
2023-11-12 12:16:13 +0100

    Adjust eventListItem

--- app/components/EventListItem/eventListItem.tsx
+++ app/components/EventListItem/eventListItem.tsx
@@ -1,20 +1,21 @@
 import styles from './eventListItem.module.scss'
 
 interface EventListItemProps {
+  id: string;
   name: string;
   surname: string;
   email: string;
   date: Date;
 }
 
-export default function EventListItem({name, surname, email, date}: EventListItemProps) {
+export default function EventListItem({id, name, surname, email, date}: EventListItemProps) {
   return (
     <div className={styles.container}>
       <div className={styles.dataContainer}>
         <p><strong>Name:</strong> {name}</p>
         <p><strong>Surname:</strong> {surname}</p>
         <p><strong>Email:</strong> {email}</p>
-        <p><strong>Date:</strong> {date.toString()}</p>
+        <p><strong>Date:</strong> {date.toString().split('T')[0]}</p>
       </div>
     <div></div>
   </div>
