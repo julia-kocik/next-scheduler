ec8053fdd6284f6de6f1475a52c19282b4bce6cc
julia-kocik <julia.kocik@gmail.com>
2023-11-14 10:50:53 +0100

    Rename docker-compose

--- app/components/EventList/eventList.test.tsx
+++ app/components/EventList/eventList.test.tsx
@@ -8,6 +8,8 @@ import { Dispatch, SetStateAction } from 'react';
 
 it('App Router: Works with Client Components (React State)', () => {
   const setForceFetchAfterDelete: Dispatch<SetStateAction<boolean>> = jest.fn();
+  const setForceFetchAfterUpdate: Dispatch<SetStateAction<boolean>> = jest.fn();
+
   const setToastInfo: Dispatch<SetStateAction<object>> = jest.fn();
-  render(<EventList setToastInfo={setToastInfo} setForceFetchAfterDelete={setForceFetchAfterDelete} eventsData={[]} />)
+  render(<EventList setToastInfo={setToastInfo} setForceFetchAfterDelete={setForceFetchAfterDelete} setForceFetchAfterUpdate={setForceFetchAfterUpdate} eventsData={[]} />)
 })
